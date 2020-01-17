31dd9ccf1612148aafe044405f800e129b68cacf
Polina Novokreschenova <polina.novokreschenova@osinit.com>
2020-01-17 09:46:03 +0300

    init tests

---
+++ src/tests/test_environment.py
@@ -0,0 +1,18 @@
+import pytest
+
+from src.environment import (
+    validate_package,
+    create_application_environment,
+)
+
+"""
+Валидация
+
+1. Нет нужных файлов
+2. Архив тяжёлый
+3. Нужные файлы пустые 
+"""
+
+
+def test_package_validation():
+    pass
