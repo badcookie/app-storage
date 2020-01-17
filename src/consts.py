37ef890485a102b2017a8c74fe19a82d293dbf22
Polina Novokreschenova <polina.novokreschenova@osinit.com>
2020-01-17 17:06:30 +0300

    refactor project base dir

--- src/consts.py
+++ src/consts.py
@@ -2,6 +2,6 @@ from os import path, pardir
 
 APP_ID_LENGTH = 8
 MAX_PACKAGE_SIZE_MB = 5
-APP_BASE_DIR = path.abspath(path.join(pardir, pardir))
+BASE_DIR = path.abspath(path.join(pardir, pardir))
 
-DEFAULT_APPS_BASE_DIR = path.abspath(path.join(APP_BASE_DIR, pardir, 'apps'))
+DEFAULT_APPS_BASE_DIR = path.abspath(path.join(BASE_DIR, pardir, 'apps'))
