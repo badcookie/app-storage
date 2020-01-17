c6e8122b11d8a3f634070eb8b091e48c1d52b8d8
Polina Novokreschenova <polina.novokreschenova@osinit.com>
2020-01-17 14:10:23 +0300

    add env creation without pip

--- src/environment.py
+++ src/environment.py
@@ -1,6 +1,12 @@
-import pip
+import sys
+import venv
+import subprocess
+from uuid import uuid4
 from typing import List
 from zipfile import ZipFile
+from os import path, mkdir, chdir, pardir
+
+from src.consts import APP_ID_LENGTH, DEFAULT_APPS_BASE_DIR
 
 
 def validate_package(package: 'ZipFile', rules: List[dict]) -> None:
@@ -11,5 +17,22 @@ def validate_package(package: 'ZipFile', rules: List[dict]) -> None:
             raise exception
 
 
-def create_application_environment(package: 'ZipFile'):
-    pass
+def create_application_environment(package: 'ZipFile') -> str:
+    app_id = uuid4().hex[:APP_ID_LENGTH]
+
+    app_dirpath = path.join(DEFAULT_APPS_BASE_DIR, app_id)
+    mkdir(app_dirpath)
+
+    package.extractall(app_dirpath)
+
+    venv_dirpath = path.join(app_dirpath, 'venv')
+    venv.create(venv_dirpath)
+
+    # chdir(app_dirpath)
+    #
+    # subprocess.check_call(
+    #     [sys.executable, '-m', 'pip', 'install', '-r' 'requirements.txt']
+    # )
+    #
+    # chdir(pardir)
+    return app_id
