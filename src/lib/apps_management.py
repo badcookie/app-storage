85f61741bd21c0f3a47d44c4add8a4bd7c166b98
Polina Novokreschenova <polina.novokreschenova@osinit.com>
2020-02-08 15:05:22 +0300

    add main server and unit files

---
+++ src/lib/apps_management.py
@@ -0,0 +1,16 @@
+# import requests
+#
+#
+# def build_request_body(app_dir: str, app_id: str) -> None:
+#
+#     app_data = {
+#         f'{app_id}': {
+#             "type": "python 3",
+#             "path": app_dir,
+#             "module": "bikes.wsgi",
+#         }
+#     }
+
+
+# curl -X PUT --data-binary @config.json --unix-socket
+# /path/to/control.unit.sock http://localhost/config/listeners/127.0.0.1:8300
