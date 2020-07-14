4ad5020b924b676f2645b03c8a243459cb22e514
Polina Novokreschenova <polina.novokreschenova@osinit.com>
2020-07-14 09:06:48 +0300

    ref

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>GG</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
