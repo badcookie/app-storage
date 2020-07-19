e1dad2c1e8babbf4caba4d1229db0a47f653fd98
polina.novokreschenova <noverianhenon@yandex.ru>
2020-07-20 01:04:19 +0300

    update local setup

--- server/settings.py
+++ server/settings.py
@@ -14,10 +14,10 @@ class Environment(str):
 
 
 class DBSettings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int = 27017
-    DB_USERNAME: str
-    DB_PASSWORD: str
+    HOST: str
+    PORT: int = 27017
+    USER: str
+    PASSWORD: str
 
 
 class Settings(BaseSettings):
@@ -27,8 +27,6 @@ class Settings(BaseSettings):
     UNIT_PORT: int = 9000
     UNIT_HOST: str = '127.0.0.1'
     DB: 'DBSettings' = DBSettings(_env_file=env_path)
-    UNIT_IMAGE: str = 'nginx/unit:1.15.0-python3.7'
-    DB_IMAGE: str = 'mongo:latest'
     APP_ID_LENGTH: int = 8
     MAX_PACKAGE_SIZE_MB: int = 5
     APP_ID_CREATION_TRIES_COUNT: int = 5
