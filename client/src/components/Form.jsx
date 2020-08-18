f14174a83d11829b79a3e404172894c50b1808ef
Polina Novokreschenova <polina.novokreschenova@osinit.com>
2020-08-18 09:36:56 +0300

    update file input form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import axios from "axios";
+import { connect } from 'react-redux';
+
+import { actions } from '../slices';
+
+const { addApp } = actions.apps;
+const actionMap = { addApp };
 
 
 class Form extends React.Component {
@@ -7,10 +14,23 @@ class Form extends React.Component {
     this.fileInput = React.createRef();
   }
 
-  handleSubmit = (fileRef) => (event) => {
+  handleSubmit = (fileRef) => async (event) => {
     event.preventDefault();
-    const file = fileRef.current.files[0];
-    console.log(file);
+
+    const formData = new FormData(event.target);
+    console.log(event.target)
+
+    const url = `http://${document.location.hostname}:8000/applications/`;
+
+    try {
+        const response = await axios.post(url, formData, {
+            headers: {'Content-Type': 'multipart/form-data'}
+        });
+        const app = response.data;
+        this.props.addApp(app);
+    } catch (e) {
+        console.log(e);
+    }
   }
 
   render() {
@@ -18,7 +38,7 @@ class Form extends React.Component {
       <form onSubmit={this.handleSubmit(this.fileInput)}>
         <label>
           Upload file:
-          <input type="file" ref={this.fileInput} />
+          <input type="file" accept=".zip" name="zipfile" required ref={this.fileInput} />
         </label>
         <br />
         <button type="submit">Submit</button>
@@ -27,4 +47,4 @@ class Form extends React.Component {
   }
 }
 
-export default Form;
+export default connect(null, actionMap)(Form);
