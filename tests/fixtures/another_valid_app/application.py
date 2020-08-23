e7d1a2a1cb01d1806445e4663aa66c4cb6631418
Polina Novokreschenova <polina.novokreschenova@osinit.com>
2020-08-23 19:57:05 +0300

    upd fixture

--- tests/fixtures/another_valid_app/application.py
+++ tests/fixtures/another_valid_app/application.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 application = Flask(__name__)
@@ -5,8 +7,9 @@ application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
 def hello_world():
-    return 'It works too'
+    return os.getenv('TEST_ENV', 1)
+
 
 @application.route('/books/<int:book_id>/', methods=['GET', 'DELETE'])
-def some_handler(book_id)
-   return book_id
+def some_handler(book_id):
+    return book_id
